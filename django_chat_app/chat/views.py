67ef9b7f2f6446a6ed45c090eb7bc535be16c627
Anastasiia Ünal <velyka.nastya@icloud.com>
2024-01-27 15:30:51 +0100

    add chat object to Message

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Message, Chat
 
 def index(request):
-    print(request.method)    
     if request.method == 'POST':             
         print(request.POST['textmessage'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(
+            text=request.POST['textmessage'], 
+            chat=myChat, 
+            author=request.user,
+            receiver=request.user)
     return render(request, 'chat/index.html', {'username': 'Anna'})
